075205f732bf7eeb4ec58103cec9daf97fc28c76
Bilisuma <bilitade2022@gmail.com>
2025-02-15 17:14:00 +0300

    BottomNavbar Component added to each forms for easy navigation

--- userwidgets/superapp.BottomNavigationBar/modules/require/BottomNavigationBarController.js
+++ userwidgets/superapp.BottomNavigationBar/modules/require/BottomNavigationBarController.js
@@ -1,6 +1,25 @@
 define(function() {
 
-	return {
+  return {
+      
+    navigateToHome: function() {
+      let navhome = new kony.mvc.Navigation("frmHome");
+      navhome.navigate(null);
+    },
+      
+    navigateToPayment: function() {
+      let navpay =new  kony.mvc.Navigation("frmPay");
+      navpay.navigate(null);
+    },
 
-	};
+    navigateToApps: function() {
+      let navapps =new kony.mvc.Navigation("frmApps");
+      navapps.navigate(null);
+    },
+
+    navigateToProfile: function() {
+      let navprofile = new kony.mvc.Navigation("frmProfile"); 
+      navprofile.navigate(null);
+    }
+  };
 });
